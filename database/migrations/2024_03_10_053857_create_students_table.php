12e45ca2d60f7569ec195fad1a8a0db892bd881d
Francis <fraanciisq@gmail.com>
2024-03-10 14:02:52 +0800

    initial commit

---
+++ database/migrations/2024_03_10_053857_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('class_id')->constrained('classes');
+            $table->foreignId('section_id')->constrained('sections');
+            // $table->string('name');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->date('birth_date');
+            $table->binary('photo')->nullable();
+            $table->string('address')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
