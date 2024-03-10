a1d495423e33041d2713bdb5fcb073a415a00393
Francis <fraanciisq@gmail.com>
2024-03-10 16:09:16 +0800

    fixed-/students

--- app/Livewire/ListStudents.php
+++ app/Livewire/ListStudents.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
+
 
 class ListStudents extends Component
 {
+    #[Layout('layouts.app')]
     public function render()
     {
         return view('livewire.list-students');
