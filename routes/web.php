43ec8f28d5b3cfa7995afc5b54653752599bdeef
Francis <fraanciisq@gmail.com>
2024-03-10 15:50:16 +0800

    logged

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Livewire\ListStudents;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/students', ListStudents::class)->name('students.index');
 });
 
 require __DIR__.'/auth.php';
