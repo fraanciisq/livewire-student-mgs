50154ee0f58da2d299b045ec6a46f446ea9117a5
Francis <fraanciisq@gmail.com>
2024-03-11 22:19:16 +0800

    add-edit-students

--- resources/views/livewire/list-students.blade.php
+++ resources/views/livewire/list-students.blade.php
@@ -10,7 +10,14 @@
                         A list of all the Students.
                     </p>
                 </div>
+                <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                    <a class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+                        Add Student
+                    </a>
+                </div> 
             </div>
+
+            
 
             <div class="mt-8 flex flex-col">
                 <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -107,6 +114,9 @@
                                             </td>
                                             <td
                                                 class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                <a class="text-indigo-600 hover:text-indigo-900">
+                                                    Edit
+                                                </a>
                                                 <button wire:confirm="Are you sure you want to delete this record?"
                                                     wire:click="deleteStudent({{ $student->id }})"
                                                     class="ml-2 text-indigo-600 hover:text-indigo-900">
