50f83def37644127d004f0c345cccb0018694d01
Francis <fraanciisq@gmail.com>
2024-03-11 22:52:35 +0800

    no-func-createstuds

---
+++ app/Livewire/CreateStudent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreateStudent extends Component
+{
+    public function render()
+    {
+        return view('livewire.create-student');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 use App\Livewire\ListStudents;
 use App\Http\Controllers\ProfileController;
+use App\Livewire\CreateStudent;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,6 +19,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/students', ListStudents::class)->name('students.index');
+    Route::get('/students/create', CreateStudent::class)->name('students.create');
 });
 
 require __DIR__.'/auth.php';
