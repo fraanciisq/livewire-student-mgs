8bc3763c26b5731daee20cb6a3f9c87359c6df6c
Francis <fraanciisq@gmail.com>
2024-03-19 20:43:25 +0800

    latest-working-login-page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,12 +16,10 @@
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
-
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
@@ -33,16 +31,28 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+        <div class="flex items-center justify-between mt-4">
+            <div>
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+            </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <div>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="mr-3">Sign up</a>
+                @endif
+
+                <x-primary-button class="mr-3">
+                    {{ __('Log in') }}
+                </x-primary-button>
+
+                
+          
+            </div>
         </div>
     </form>
 </x-guest-layout>
